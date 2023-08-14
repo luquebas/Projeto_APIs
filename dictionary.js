b279b2a720e42da5862f22b7381aed5f49e85e44
luquebas <133112453+luquebas@users.noreply.github.com>
2023-08-13 21:31:32 -0300

    Add files via upload

---
+++ dictionary.js
@@ -0,0 +1,33 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById('results');
+const sound = document.getElementById('sound');
+const btnSearch = document.getElementById('search-btn');
+
+btnSearch.addEventListener('click', () => {
+    let inpSearch = document.getElementById("inpSearch").value;
+    fetch(`${url}${inpSearch}`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        result.innerHTML = `
+        <div class="word">
+            <h3>${inpSearch}</h3>
+            <button id="loud-speaker" onclick="playSound()">
+                <i class="fas fa-volume-up"></i>
+            </button>
+        </div>
+        <div class="detalhes">
+            <p>${data[0].meanings[0].partOfSpeech}</p>
+            <p>${data[0].phonetic}</p>
+        </div>
+            <p class="significado">${data[0].meanings[0].definitions[0].definition}</p>
+            <p class="exemplo">${data[0].meanings[0].definitions[0].example}</p>
+        </div>`
+        sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+        console.log(sound);
+    });
+});
+
+function playSound() {
+    sound.play();
+}
